ff9505e8de0670d6db5406d3b37f72d51e2e5cd5
Peter Kreppein <pmkreppein@gmail.com>
2019-01-11 06:49:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-$(document).ready(function (){
-});
-function searchRepositories() {
-    const searchTerms = $('#searchTerms').val();
-    const uri = 'https://api.github.com/search/repositories?q=' + searchTerms
-    $.get(uri, function(data) {
-        console.log(data.items)
-        $('#results').html(renderSearchResults(data.items))
-    }).fail(function(error) {displayError(error)})
-};
 
-function displayError(error) {
-    $('#errors').html("There is an error." + error);
+var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
+
+var renderCommit = (commit) => {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
 }
 
-function renderSearchResults(result) {
-    const src = $('#repository-template').html();
-    const template = Handlebars.compile(src);
-    return template(result);
+var renderCommits = (data) => {
+  let result = data.map((commit)=>renderCommit(commit)).join('')
+  return `<ul>${result}</ul>`
 }
 
-function showCommits(el) {
-    const repo = el.dataset.repo;
-    const uri = 'https://api.github.com/repos/' + repo +'/commits';
-    $.get(uri, function(data) {
-        console.log(data)
-        $('#details').html(renderCommits(data))
+var showCommits = (el) => {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+    $('#details').html(renderCommits(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
+var renderSearchResult = (result) => {
+  return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+}
+
+var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
+
+var searchRepositories = () => {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      $('#results').html(renderSearchResults(data))
+    }).fail(error => {
+      displayError()
     })
 }
 
-function renderCommits(commits) {
-    const src = $('#commits-template').html();
-    const template = Handlebars.compile(src);
-    return template(commits);
-}
+$(document).ready(function (){
+})
